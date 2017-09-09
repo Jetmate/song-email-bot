4b31379bb18ccdd02ea61d31d9db8452428517eb
Jetmate <antonoutkine@gmail.com>
2017-09-09 15:55:00 -0500

    Add authorization / remove JS wrapper  * Use fetch + OAUTH2 to authorize user  * Use fetch to retrieve info - no more JS wrapper  * All info is extracted from 'client_info.json'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,79 @@
 import express from 'express'
 import path from 'path'
+import fs from 'fs'
+import querystring from 'querystring'
+import nodeSchedule from 'node-schedule'
+import request from 'request'
 
-const WWW = path.join(__dirname, 'www')
+const WWW = path.join(__dirname, '../www')
+let clientSecret, clientId, USER, PLAYLIST
+fs.readFile(path.join(__dirname, '../client_info.json'), 'utf8', (err, data) => {
+  if (err) throw err
+  const results = JSON.parse(data)
+  clientSecret = results.clientSecret
+  clientId = results.clientId
+  USER = results.user
+  PLAYLIST = results.playlist
+})
+const redirectUri = 'http://0.0.0.0:4000/callback'
 
 const app = express()
-const server = app.listen(3000)
-app.use(express.static(WWW))
+app.set('views', WWW)
+const server = app.listen(4000, '0.0.0.0')
+let job = new nodeSchedule.Job()
 
-console.log('RUNNING ON http://127.0.0.1:3000/')
+app.get('/', (req, res) => {
+  let scope = 'playlist-read-private';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri
+    })
+  )
+})
+
+app.get('/callback', (req, res, next) => {
+  const code = req.query.code
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: redirectUri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+    },
+    json: true
+  }
+
+  request.post(authOptions, (err, resp, body) => {
+    if (err) throw err
+    console.log(2)
+
+    const accessToken = body.access_token
+    // job.schedule('0 20 * * 0', () => {
+      main(accessToken)
+    // })
+  })
+
+  res.sendFile('callback.html', { root: WWW })
+})
+
+function main(accessToken) {
+  var options = {
+    url: `https://api.spotify.com/v1/users/${USER}/playlists/${PLAYLIST}/tracks`,
+    headers: { 'Authorization': 'Bearer ' + accessToken },
+    json: true
+  }
+
+  request.get(options, function(err, res, body) {
+    console.log(body);
+  })
+}
+
+
+console.log('RUNNING ON http://0.0.0.0:4000/')
