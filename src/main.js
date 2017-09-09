c6f902666a9c7d64ce5d756b238f3dfb9f0912ac
Jetmate <antonoutkine@gmail.com>
2017-09-09 17:08:06 -0500

    Add email infrastructure with nodemailer

--- src/main.js
+++ src/main.js
@@ -4,16 +4,26 @@ import fs from 'fs'
 import querystring from 'querystring'
 import nodeSchedule from 'node-schedule'
 import request from 'request'
+import nodemailer from 'nodemailer'
 
 const WWW = path.join(__dirname, '../www')
-let clientSecret, clientId, USER, PLAYLIST
+let SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID, SPOTIFY_USERNAME, PLAYLIST, GMAIL_USERNAME, GMAIL_PASSWORD, EMAIL_RECIPIENT, EMAIL_SUBJECT, GMAIL_ACCESS_TOKENl, GMAIL_REFRESH_TOKEN, GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_ACCESS_TOKEN
 fs.readFile(path.join(__dirname, '../client_info.json'), 'utf8', (err, data) => {
   if (err) throw err
   const results = JSON.parse(data)
-  clientSecret = results.clientSecret
-  clientId = results.clientId
-  USER = results.user
+  SPOTIFY_CLIENT_SECRET = results.spotifyClientSecret
+  SPOTIFY_CLIENT_ID = results.spotifyClientId
+  SPOTIFY_USERNAME = results.spotifyUsername
   PLAYLIST = results.playlist
+
+  GMAIL_USERNAME = results.gmailUsername
+  GMAIL_ACCESS_TOKEN = results.gmailAccessToken
+  GMAIL_REFRESH_TOKEN = results.gmailRefreshToken
+  GMAIL_CLIENT_ID = results.gmailClientId
+  GMAIL_CLIENT_SECRET = results.gmailClientSecret
+
+  EMAIL_RECIPIENT = results.emailRecipient
+  EMAIL_SUBJECT = results.emailSubject
 })
 const redirectUri = 'http://0.0.0.0:4000/callback'
 
@@ -27,7 +37,7 @@ app.get('/', (req, res) => {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
-      client_id: clientId,
+      client_id: SPOTIFY_CLIENT_ID,
       scope: scope,
       redirect_uri: redirectUri
     })
@@ -45,7 +55,7 @@ app.get('/callback', (req, res, next) => {
       grant_type: 'authorization_code'
     },
     headers: {
-      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+      'Authorization': 'Basic ' + (new Buffer(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64'))
     },
     json: true
   }
@@ -55,12 +65,15 @@ app.get('/callback', (req, res, next) => {
 
     const accessToken = body.access_token
     const options = {
-      url: `https://api.spotify.com/v1/users/${USER}/playlists/${PLAYLIST}/tracks`,
+      url: `https://api.spotify.com/v1/users/${SPOTIFY_USERNAME}/playlists/${PLAYLIST}/tracks`,
       headers: { 'Authorization': 'Bearer ' + accessToken },
       json: true
     }
+    console.log(SPOTIFY_USERNAME, PLAYLIST, accessToken)
 
     request.get(options, function(err, res, body) {
+      if (err) throw err
+
       const songs = body.items.map(element => element.track)
       let playedSongs = []
       // job.schedule('0 20 * * 0', () => {
@@ -79,7 +92,32 @@ app.get('/callback', (req, res, next) => {
 })
 
 function main(song) {
-  console.log(song.name)
+  console.log(SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID, SPOTIFY_USERNAME, PLAYLIST, GMAIL_USERNAME, GMAIL_PASSWORD, EMAIL_RECIPIENT, EMAIL_SUBJECT, GMAIL_ACCESS_TOKENl, GMAIL_REFRESH_TOKEN, GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_ACCESS_TOKEN)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: GMAIL_USERNAME,
+      clientId: GMAIL_CLIENT_ID,
+      clientSecret: GMAIL_CLIENT_SECRET,
+      accessToken: GMAIL_ACCESS_TOKEN,
+      refreshToken: GMAIL_REFRESH_TOKEN
+    }
+  })
+
+  const messageOptions = {
+    from: GMAIL_USERNAME,
+    to: EMAIL_RECIPIENT,
+    subject: EMAIL_SUBJECT,
+    text: `test`
+  }
+
+  transporter.sendMail(messageOptions, (err, info) => {
+    if (err) throw err
+    else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
 }
 
 
